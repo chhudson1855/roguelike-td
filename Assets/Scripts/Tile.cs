82658e5335c22e9999f9730129e3081c3915955e
munhty <mhtran483@gmail.com>
2026-01-13 16:57:39 -0600

    Coded Cursor
    Coded Script for cursor in Tile

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,28 +1,82 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public List<GameObject> occupiedBy = new List<GameObject>();
-    public int xCordinate, yCordinate = 0;
+    
+    public int xCordinate;
+    public int yCordinate;
+
+    public bool Walkable = true;
     public bool BuiltOn = false;
-    public bool Walkable = false;
-    public void Init(int x, int y, List<GameObject> occupants)
+    // Allow holding down for movement
+    [SerializeField] private bool isHoldDown = false;
+    // Hangtime (time to move)
+    [SerializeField] private float hTime = 0.1f;
+    // Size of grid 
+    [SerializeField] private float gridSize = 0.35f;
+
+    private bool isMoving = false;
+
+    private void Update()
     {
-        xCordinate = x;
-        yCordinate = y;
-        occupiedBy = occupants;
-    }
-    void Start()
-    {
-        
+        // If already moving, don't accept input
+        if (isMoving) return;
+
+        System.Func<KeyCode, bool> inputFunction;
+
+        if (isHoldDown)
+        {
+            // GetKey repeatedly fires
+            inputFunction = Input.GetKey;
+        }
+        else
+        {
+            // GetKeyDown fires once per keypress
+            inputFunction = Input.GetKeyDown;
+        }
+
+        // If input func active, move in direction
+        if (inputFunction(KeyCode.UpArrow))
+        {
+            StartCoroutine(Move(Vector2.up));
+        }
+        else if (inputFunction(KeyCode.DownArrow))
+        {
+            StartCoroutine(Move(Vector2.down));
+        }
+        else if (inputFunction(KeyCode.LeftArrow))
+        {
+            StartCoroutine(Move(Vector2.left));
+        }
+        else if (inputFunction(KeyCode.RightArrow))
+        {
+            StartCoroutine(Move(Vector2.right));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Move(Vector2 direction)
     {
-        
+        isMoving = true;
+
+        Vector2 startPos = transform.position;
+        Vector2 endPos = startPos + (direction * gridSize);
+
+        float elapsed = 0;
+
+        while (elapsed < hTime)
+        {
+            
+            elapsed += Time.DeltaTime; 
+            
+            float percent = elapsed / hTime;
+            transform.position = Vector2.Lerp(startPos, endPos, percent);
+            yield return null;
+        }
+
+       
+        transform.position = endPos;
+
+        isMoving = false;
     }
 }
